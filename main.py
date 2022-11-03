39710aa15da32ff82337054cf72b94a83ff1db89
Sir-Buy <65486996+Sir-Buy@users.noreply.github.com>
2022-11-03 22:33:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import matplotlib.pyplot as plt
+
 x = 0
 y = 0
 durchläufe = 100
@@ -9,24 +10,29 @@ geldstand = []
 wallet = 100
 round = []
 einsatz = 1
-print(input)
+
 while y <= durchläufe and wallet - einsatz > 0:
     round.append(y)
     rand = random.choice(odds)
     input.append(rand)
+
     if rand == 1:
         wallet = wallet + einsatz
         einsatz = 1
         geldstand.append(wallet)
+
     if rand == 2:
         wallet = wallet - einsatz
         einsatz = einsatz * 2
         geldstand.append(wallet)
+
     y = y + 1
+    
 print(wallet)
 print(input)
 print(geldstand)
 print(round)
+
 plt.plot(round, geldstand)
 plt.xlabel('Runden')
 plt.ylabel('Geld')
