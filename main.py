a368be8790448fe9ad57ce2c6778b54ad72b8875
Sir-Buy <65486996+Sir-Buy@users.noreply.github.com>
2022-11-03 22:35:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,33 +2,33 @@ import random
 import matplotlib.pyplot as plt
 x = 0
 y = 0
-durchläufe = 100
+rounds = 100
 odds = [1, 2]
 input = []
-geldstand = []
+balance = []
 wallet = 100
 round = []
-einsatz = 1
+bet = 1
 print(input)
-while y <= durchläufe and wallet - einsatz > 0:
+while y <= rounds and wallet - bet > 0:
     round.append(y)
     rand = random.choice(odds)
     input.append(rand)
     if rand == 1:
-        wallet = wallet + einsatz
-        einsatz = 1
-        geldstand.append(wallet)
+        wallet = wallet + bet
+        bet = 1
+        balance.append(wallet)
     if rand == 2:
-        wallet = wallet - einsatz
-        einsatz = einsatz * 2
-        geldstand.append(wallet)
+        wallet = wallet - bet
+        bet = bet * 2
+        balance.append(wallet)
     y = y + 1
 print(wallet)
 print(input)
-print(geldstand)
+print(balance)
 print(round)
-plt.plot(round, geldstand)
-plt.xlabel('Runden')
-plt.ylabel('Geld')
+plt.plot(round, balance)
+plt.xlabel('Round')
+plt.ylabel('Money')
 plt.title('Gambling strat')
 plt.show()
